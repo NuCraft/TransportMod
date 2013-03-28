f6940a6f9356a39f90165dfdbefa43eef6258944
BlueTears <maziarkj@my.erau.edu>
2013-03-28 17:46:08 -0400

    Has content in the item

--- common/com/bluetears/transportmod/items/SimpleCombustionEngine.java
+++ common/com/bluetears/transportmod/items/SimpleCombustionEngine.java
@@ -1,5 +1,23 @@
 package com.bluetears.transportmod.items;
 
-public class SimpleCombustionEngine {
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class SimpleCombustionEngine extends Item
+{
+
+
+public SimpleCombustionEngine(int id)
+{
+         super(id);
+         setCreativeTab(CreativeTabs.tabMisc);
+}
+
+@Override
+public void updateIcons(IconRegister iconRegister)
+{
+         iconIndex = iconRegister.registerIcon("transportmod:simplecombustionengine");
+}
 
 }
