0923817a62c08f75da8237cd8704f1b9343598ac
BlueTears <maziarkj@my.erau.edu>
2013-03-29 19:49:44 -0400

    More Ores

---
+++ common/com/bluetears/transportmod/blocks/GuiltaliumOre.java
@@ -0,0 +1,25 @@
+package com.bluetears.transportmod.blocks;
+
+import com.bluetears.transportmod.TransportMod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class GuiltaliumOre extends Block {
+
+        public GuiltaliumOre (int id, Material iron) {
+                super(id, iron);
+                setHardness(4.0F); // 33% harder than diamond
+                setStepSound(Block.soundStoneFootstep);
+                setCreativeTab(TransportMod.transportmodtab);
+        }
+        
+        public void registerIcons(IconRegister par1IconRegister)
+        {
+                 this.blockIcon = par1IconRegister.registerIcon("transportmod:guiltaliumore");
+        }
+
+        
+        
+}
